b32007d4633ff0bf780a4a8659e23df9521c3377
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-03 05:02:06 +0300

    add whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # python3 -m venv venv 
 # source venv/bin/activate
-# pip install onnxruntime numpy librosa torch torchvision torchaudio
+# pip install onnxruntime numpy librosa torch torchvision torchaudio faster-whisper
 # wget https://github.com/pengzhendong/pyannote-onnx/blob/master/pyannote_onnx/segmentation-3.0.onnx
 # wget https://github.com/k2-fsa/sherpa-onnx/releases/download/speaker-recongition-models/wespeaker_en_voxceleb_CAM++.onnx
 # wget https://github.com/thewh1teagle/sherpa-rs/releases/download/v0.1.0/sam_altman.wav
@@ -10,8 +10,10 @@ from segment import get_segments
 from identify import SpeakerEmbeddingManager
 from extract import EmbeddingExtractor
 from common import read_wav
+from faster_whisper import WhisperModel
 
 if __name__ == '__main__':
+    model = WhisperModel("tiny", compute_type="int8", device='auto')
     samples, sample_rate = read_wav('sam_altman.wav')
     
     embedding_manager = SpeakerEmbeddingManager(3)
@@ -21,7 +23,15 @@ if __name__ == '__main__':
         start_sample = int(segment['start'] * sample_rate)
         end_sample = int(segment['end'] * sample_rate)
         segment_samples = samples[start_sample:end_sample]
+        
+        # Text
+        whisper_result = model.transcribe(segment_samples, language='en')
+        text = ' '.join(i.text for i in whisper_result[0])
+        segment['text'] = text
+        
+        # Speaker
         embedding = extractor.compute(segment_samples, sample_rate)
         speaker = embedding_manager.get_speaker(embedding, threshold=0.4)
         segment['speaker'] = speaker
+        
         print(segment)
