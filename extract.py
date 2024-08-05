788fe8c52f8ff324208c1ddba82b704852a4112c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-08-06 02:26:39 +0300

    feat: fbank using kaldi native fbank

--- extract.py
+++ extract.py
@@ -1,10 +1,8 @@
 import numpy as np
-import torch
 import kaldi_native_fbank as knf
-import torchaudio.compliance.kaldi as kaldi
 from common import init_session
 
-def compute_fbank_kaldi_native(samples: np.ndarray, sample_rate: int) -> np.ndarray:
+def compute_fbank(samples: np.ndarray, sample_rate: int) -> np.ndarray:
     opts = knf.FbankOptions()
     opts.frame_opts.dither = 0
     opts.frame_opts.samp_freq = sample_rate
@@ -28,46 +26,12 @@ def compute_fbank_kaldi_native(samples: np.ndarray, sample_rate: int) -> np.ndar
     
     return features
 
-
-def compute_fbank(samples,
-                  sample_rate,
-                  num_mel_bins=80,
-                  frame_length=25,
-                  frame_shift=10,
-                  dither=0.0):
-    """Extract fbank, similar to the one in wespeaker.dataset.processor,
-       While integrating the wave reading and CMN.
-    """
-    waveform = torch.tensor(samples, dtype=torch.float32)
-    
-    # Add channel dimension if waveform is 1D
-    if waveform.ndim == 1:
-        waveform = waveform.unsqueeze(0)
-    
-    waveform = waveform * (1 << 15)  # Scale to 16-bit range
-    
-    mat = kaldi.fbank(waveform,
-                      num_mel_bins=num_mel_bins,
-                      frame_length=frame_length,
-                      frame_shift=frame_shift,
-                      dither=dither,
-                      sample_frequency=sample_rate,
-                      window_type='hamming',
-                      use_energy=False)
-    
-    # CMN, without CVN
-    mat = mat - torch.mean(mat, dim=0)
-    return mat
-
 class EmbeddingExtractor:
     def __init__(self, model_path: str) -> None:
         self.session = init_session(model_path)
     
-    def compute(self, samples: np.ndarray, sample_rate: int, use_native_fbank = True) -> np.ndarray:
-        if use_native_fbank:
-            feats = compute_fbank_kaldi_native(samples, sample_rate)
-        else:
-            feats = compute_fbank(samples, sample_rate)
+    def compute(self, samples: np.ndarray, sample_rate: int) -> np.ndarray:
+        feats = compute_fbank(samples, sample_rate)
         feats = np.expand_dims(feats, axis=0) # add batch dimension
 
         embeddings = self.session.run(output_names=['embs'],
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 python3 -m venv venv 
 source venv/bin/activate
-pip install onnxruntime numpy librosa torch torchvision torchaudio kaldi-native-fbank
+pip install onnxruntime numpy librosa kaldi-native-fbank
 wget https://github.com/pengzhendong/pyannote-onnx/raw/master/pyannote_onnx/segmentation-3.0.onnx
 wget https://github.com/k2-fsa/sherpa-onnx/releases/download/speaker-recongition-models/wespeaker_en_voxceleb_CAM++.onnx
 wget https://github.com/thewh1teagle/sherpa-rs/releases/download/v0.1.0/5_speakers.wav
@@ -21,13 +21,12 @@ if __name__ == '__main__':
     extractor = EmbeddingExtractor('wespeaker_en_voxceleb_CAM++.onnx')
     segments = get_segments('segmentation-3.0.onnx', samples, sample_rate)
     
-    print('--- Pytorch kaldi fbank ---')
     embedding_manager = SpeakerEmbeddingManager(num_speakers)
     for segment in segments:
         start_sample = int(segment['start'] * sample_rate)
         end_sample = int(segment['end'] * sample_rate)
         segment_samples = samples[start_sample:end_sample]
-        embedding = extractor.compute(segment_samples, sample_rate, use_native_fbank=False)
+        embedding = extractor.compute(segment_samples, sample_rate)
 
         speaker = embedding_manager.get_speaker(embedding, threshold=0.5)
         if not speaker and len(embedding_manager.get_all_speakers()):
@@ -36,17 +35,3 @@ if __name__ == '__main__':
         segment['speaker'] = speaker
         print(segment)
         
-    print('--- Kaldi Native fbank ---')
-    embedding_manager = SpeakerEmbeddingManager(num_speakers)
-    for segment in segments:
-        start_sample = int(segment['start'] * sample_rate)
-        end_sample = int(segment['end'] * sample_rate)
-        segment_samples = samples[start_sample:end_sample]
-        embedding = extractor.compute(segment_samples, sample_rate, use_native_fbank=True)
-
-        speaker = embedding_manager.get_speaker(embedding, threshold=0.5)
-        if not speaker and len(embedding_manager.get_all_speakers()):
-            speaker = embedding_manager.get_speaker(embedding, threshold=0)
-            
-        segment['speaker'] = speaker
-        print(segment)
